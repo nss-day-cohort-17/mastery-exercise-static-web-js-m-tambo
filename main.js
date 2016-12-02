129059a60c98065f9d768ec6a0e73e56e68d115d
Michael Tambornino <michael.tambornino@gmail.com>
2016-12-01 20:30:26 -0600

    js fiddling, still no idea

--- main.js
+++ main.js
@@ -2,10 +2,18 @@ console.log("is this thing on?")
 
 
 
-var tHeight = document.getElementById("treeHeight").value;
+var treeHeight = document.getElementById("treeHeight").input;
 
-var tChar = document.getElementById("treeChar").input;
+var treeChar = document.getElementById("treeChar").input;
 
-function growTree () {
-   console.log(tHeight, tChar);
+var button = document.getElementById("grow");
+
+function growTree(treeHeight, treeChar) {
+
 }
+
+button.addEventListener('click', growTree(), false)
+
+
+
+console.log(treeHeight, treeChar);
