9e3ba0afa0514c93a1020b59c612b85c29431e68
Michael Tambornino <michael.tambornino@gmail.com>
2016-12-02 11:42:38 -0600

    define object with 2 key/value pairs

--- main.js
+++ main.js
@@ -4,17 +4,17 @@ console.log("is this thing on?")
 // get variables
 
 function getTreeObj() {
-   var treeObj = {};
    var treeHeight = document.getElementById("treeHeight").value;
    var treeChar = document.getElementById("treeChar").value;
-   treeObj.
+   var treeObj = {tHeight: treeHeight, tChar: treeChar}
 }
+
 
 
 // define function
 function growTree(treeObj) {
-   for(var i = 0; i <= treeObj.treeHeight; i++) {
-     newTree += " ".repeat(treeObj.treeHeight - i) + treeObj.treeChar.repeat(2*i + 1) + "\n";
+   for(var i = 0; i <= treeObj.tHeight; i++) {
+     newTree += " ".repeat(treeObj.tHeight - i) + treeObj.tChar.repeat(2*i + 1) + "\n";
    }
   console.log(newTree);
 }
