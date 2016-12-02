bd53de5bc3c2d5e158e6561cec818c0608f2196d
Michael Tambornino <michael.tambornino@gmail.com>
2016-12-02 14:59:46 -0600

    added obj specs to button listener

--- main.js
+++ main.js
@@ -6,20 +6,23 @@ console.log("is this thing on?")
 function getTreeSpecs() {
    var treeHeight = document.getElementById("treeHeight").value;
    var treeChar = document.getElementById("treeChar").value;
-   var treeSpecs = {tHeight: treeHeight, tChar: treeChar}
+   var treeSpecs = {tHeight: treeHeight, tChar: treeChar};
+   return treeSpecs;
 }
 
 // define function
 function growTree(Obj) {
+   var newTree ="";
    getTreeSpecs();
    for(var i = 0; i <= Obj.tHeight; i++) {
      newTree += " ".repeat(Obj.tHeight - i) + Obj.tChar.repeat(2*i + 1) + "\n";
    }
-  console.log(newTree);
+  console.log(newTree)
 }
 
 document.getElementById("grow").addEventListener('click', function() {
-   growTree(treeSpecs)
+   var treeSpecs = getTreeSpecs();
+   growTree(treeSpecs);
 })
 
 document.getElementById("grow").addEventListener('keypress', function() {
@@ -32,17 +35,3 @@ document.getElementById("grow").addEventListener('keypress', function() {
 // }
 // submit.addEventListener()
 // submit.addEventListener()
-
-
-
-
-
-// testing
-var treeHeight = document.getElementById("treeHeight").value;
-console.log(treeHeight);
-
-
-
-
-// listen for.... the two parameters?
-// button.addEventListener('click', growTree(treeHeight, treeChar), false)
